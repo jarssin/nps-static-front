89324f0af110648bd0569add38f8f010019e6398
igormaia01 <igor.maia@syngenta.com>
2025-06-13 17:59:43 -0300

    fix: fix lint sintax

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,7 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       '@typescript-eslint/no-unused-vars': 'off',
-      'no-unused-imports': 'error', 
+      'no-unused-imports': 'error',
+    },
   }
 );
